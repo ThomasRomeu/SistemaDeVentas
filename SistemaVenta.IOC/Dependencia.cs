603b17a5415be3634e0ad19389e3f86a69afa149
Thomas <losromeu@hotmail.com>
2023-09-17 09:59:34 -0300

    Se implementa la logica del envio de correos

--- SistemaVenta.IOC/Dependencia.cs
+++ SistemaVenta.IOC/Dependencia.cs
@@ -9,8 +9,8 @@ using SistemaVenta.DAL.DBContext;
 using Microsoft.EntityFrameworkCore;
 using SistemaVenta.DAL.Implementacion;
 using SistemaVenta.DAL.Interfaces;
-//using SistemaVenta.BLL.Implementacion;
-//using SistemaVenta.BLL.Interfaces;
+using SistemaVenta.BLL.Implementacion;
+using SistemaVenta.BLL.Interfaces;
 
 
 namespace SistemaVenta.IOC
@@ -26,6 +26,8 @@ namespace SistemaVenta.IOC
 
             services.AddTransient(typeof(IGenericRepository<>),typeof(GenericRepository<>));
             services.AddScoped<IVentaRepository, VentaRepository>();
+
+            services.AddScoped<ICorreoService, CorreoService>();
 
         }
     }
